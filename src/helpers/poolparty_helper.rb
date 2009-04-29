f5cfd611d66f68932f5d7292c87f8ea7b53718a5
Nate Murray <nate@natemurray.com>
2009-04-29 14:33:11 -0700

    updated tip box to have a nice lil img

--- src/helpers/poolparty_helper.rb
+++ src/helpers/poolparty_helper.rb
@@ -2,4 +2,9 @@ module PoolpartyHelper
   def define_term(term, &block)
      "<div class='term'><a name='#{term}' class='term-name'>#{term}</a>\n<div class='term-def'>#{capture_haml(&block).chomp}</div></div>\n"
   end
+
+  def tip_box(&block)
+    crab_img_url = "http://fairchild.github.com/poolparty/images/site/i/examples.png"
+    "<div class='tip-box'><div class='crab-wrapper'><img src='#{crab_img_url}' class='crab'/></div>#{capture_haml(&block).chomp}</div>\n"
+  end
 end
